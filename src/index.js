57bd193be5f69472bf024fb1465c1641869f4bb8
shuang zheng <shuangzheng16@gmail.com>
2021-11-04 12:25:49 -0700

    test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const Butterfly = require("./scripts/butterfly").default;
     const timeElement = document.getElementById('timer');
     const resultButton = document.getElementById('result');
     const resultShow = document.getElementById('result-show');
+    //fadsf
 
     let rowNum = 5;
     let colNum = 5;
